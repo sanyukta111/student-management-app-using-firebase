b7f12e988a440014f30a446003d8702102d80ad6
sanyukta111 <62386991+sanyukta111@users.noreply.github.com>
2021-12-19 00:02:40 +0530

    Add files via upload

---
+++ app/src/main/java/utils/BotResponse.kt
@@ -0,0 +1,108 @@
+package com.example.firebase_register.utils
+
+import com.example.firebase_register.utils.Constants.OPEN_EXAM_FORM
+import com.example.firebase_register.utils.Constants.OPEN_GOOGLE
+import com.example.firebase_register.utils.Constants.OPEN_RESULT
+import com.example.firebase_register.utils.Constants.OPEN_SEARCH
+import java.sql.Date
+import java.sql.Timestamp
+import java.text.SimpleDateFormat
+
+object BotResponse {
+
+    fun basicResponses(_message: String): String {
+
+        val random = (0..2).random()
+        val message =_message.toLowerCase()
+
+        return when {
+
+            //Flips a coin
+            message.contains("flip") && message.contains("coin") -> {
+                val r = (0..1).random()
+                val result = if (r == 0) "heads" else "tails"
+
+                "I flipped a coin and it landed on $result"
+            }
+
+            //Math calculations
+            message.contains("solve") -> {
+                val equation: String? = message.substringAfterLast("solve")
+                return try {
+                    val answer = SolveMath.solveMath(equation ?: "0")
+                    "$answer"
+
+                } catch (e: Exception) {
+                    "Sorry, I can't solve that."
+                }
+            }
+
+            //Hello
+            message.contains("hello") -> {
+                when (random) {
+                    0 -> "Hello there!"
+                    1 -> "Sup"
+                    2 -> "Buongiorno!"
+                    else -> "error" }
+            }
+
+            //How are you?
+            message.contains("how are you") -> {
+                when (random) {
+                    0 -> "I'm doing fine, thanks!"
+                    1 -> "I'm hungry..."
+                    2 -> "Pretty good! How about you?"
+                    else -> "error"
+                }
+            }
+
+            //What time is it?
+            message.contains("time") && message.contains("?")-> {
+                val timeStamp = Timestamp(System.currentTimeMillis())
+                val sdf = SimpleDateFormat("dd/MM/yyyy HH:mm")
+                val date = sdf.format(Date(timeStamp.time))
+
+                date.toString()
+            }
+
+            //Open Google
+            message.contains("open") && message.contains("google")-> {
+                OPEN_GOOGLE
+            }
+
+            message.contains("exam") && message.contains("exam dates")-> {
+                when (random) {
+                    0 -> "Tentative dates are 14 june"
+                    1 -> "Official time table is yet to come. But tentative dates are 14 june"
+                    else -> "error"
+                }
+            }
+
+            //Search on the internet
+            message.contains("search")-> {
+                OPEN_SEARCH
+            }
+
+            //sppu
+            message.contains("sppu")-> {
+                OPEN_EXAM_FORM
+            }
+
+            //exam result
+            message.contains("sppu")-> {
+                OPEN_RESULT
+            }
+
+
+            //When the programme doesn't understand...
+            else -> {
+                when (random) {
+                    0 -> "I don't understand..."
+                    1 -> "Try asking me something different"
+                    2 -> "Idk"
+                    else -> "error"
+                }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/utils/Constants.kt
@@ -0,0 +1,13 @@
+package com.example.firebase_register.utils
+
+object Constants {
+
+    const val SEND_ID = "SEND_ID"
+    const val RECEIVE_ID = "RECEIVE_ID"
+
+    const val OPEN_GOOGLE = "Opening Google..."
+    const val OPEN_SEARCH = "Searching..."
+    const val OPEN_EXAM_FORM = "Opening Sppu..."
+    const val OPEN_RESULT = "Opening sppu result..."
+    const val OPEN_ASSIGNMENT = "opening assignment page...."
+}
